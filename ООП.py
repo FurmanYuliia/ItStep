9e95641744db72fadfda6ecdabe4c8de4ed5fcdb
FurmanYuliia <130824040+FurmanYuliia@users.noreply.github.com>
2025-03-15 11:11:45 +0200

    Class

--- ООП.py
+++ ООП.py
@@ -1,12 +1,38 @@
-class Car:
-    #speed=110
-    def __init__(self,speed):
-        self.speed=speed
-    def info(self):
-        print("Швидкість авто: ",self.speed)
-sp=int(input('Максимальна шв авто: '))
-auto=Car(sp)
-#print("Швидкість авто:",auto.speed)
-auto.info()
-auto2=Car(180)
-auto2.info()
+#class Car:
+ #   #speed=110
+  #  def __init__(self,speed):
+   #     self.speed=speed
+    #def info(self):
+     #   print("Швидкість авто: ",self.speed)
+#sp=int(input('Максимальна шв авто: '))
+#auto=Car(sp)
+##print("Швидкість авто:",auto.speed)
+#auto.info()
+#auto2=Car(180)
+#auto2.info()
+
+
+#class Pupils:
+    #count=0
+    #def __init__(self,name,height):
+        #self.name=name
+        #self.height=height
+        #Pupils.count+=1
+    #def __str__(self):
+        #print("Ім'я учасника:", self.name,",Зріст:", self.height)
+    #def __bool__(self):
+        #return self.name!=None
+    #def __len__(self):
+        #return self.height
+
+#p1=Pupils("Ігор", 155)
+#p1.__str__()
+#p2=Pupils("Оля", 158)
+#p2.__str__()
+#p3=Pupils("Петро", 162)
+#p3.__str__()
+#print("Всього учасників:",p1.count)
+##print(p1.count(p1.__bool__())
+#print(bool(p2))
+##print(p3.__len__())
+#print(len(p3))
